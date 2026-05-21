2f49a84a799b5815d99026435190904bb1716835
Fersch_21 <christianfs210108@gmail.com>
2026-05-21 10:52:17 -0300

    Foi adicionada na Pasta include Biblioteca.h

---
+++ include/biblioteca.h
@@ -0,0 +1,47 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_LIVROS 500
+#define MAX_USUARIOS 500
+#define MAX_EMPRESTIMOS 2000
+#define MAX_STRING 100
+#define MAX_EMPRESTIMOS_USUARIO 3
+#define PRAZO_DEVOLUCAO 14
+
+typedef struct
+{
+    int codigo;
+    char titulo[MAX_STRING];
+    char autor[MAX_STRING];
+    int ano;
+    char genero[MAX_STRING];
+    int qtd_total;
+    int qtd_disponivel;
+    int total_emprestimos;
+} Livro;
+
+typedef struct
+{
+    int matricula;
+    char nome[MAX_STRING];
+    char curso[MAX_STRING];
+    int qtd_emprestimos_ativos;
+} Usuario;
+
+typedef struct
+{
+    int id;
+    int matricula_usuario;
+    int codigo_livro;
+    char data_retirada[11];
+    char data_prevista[11];
+    char data_devolucao[11];
+    int devolvido;
+} Emprestimo;
+
+#endif
